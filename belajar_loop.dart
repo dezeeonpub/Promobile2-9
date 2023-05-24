9758a20a895b1541d1810ac39ff7461078db5f92
dezee_ <90754841+dezeeonpub@users.noreply.github.com>
2023-05-24 19:05:08 +0700

    Add files via upload

---
+++ belajar_loop.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      String nama = "azizul"; // Ganti "Nama" dengan nama yang diinginkan
+      print("$i: $nama");
+    } else {
+      print(i);
+    }
+  }
+}
